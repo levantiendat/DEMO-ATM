aad3d94712692622b374125d56cb07eba66a124f
levantiendat <levan040669@gmail.com>
2023-04-24 18:27:56 +0700

    commit class

--- src/Account.java
+++ src/Account.java
@@ -9,4 +9,72 @@ public class Account {
     private String Type;
     private double Amount;
 
+    public Account() {
+    }
+
+    public Account(String cardID, String password, String name, Calendar dateOfBirth, String citizenID, String type, double amount) {
+        CardID = cardID;
+        Password = password;
+        Name = name;
+        DateOfBirth = dateOfBirth;
+        CitizenID = citizenID;
+        Type = type;
+        Amount = amount;
+    }
+
+    public String getCardID() {
+        return CardID;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public Calendar getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    public String getCitizenID() {
+        return CitizenID;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public double getAmount() {
+        return Amount;
+    }
+
+    public void setCardID(String cardID) {
+        CardID = cardID;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setDateOfBirth(Calendar dateOfBirth) {
+        DateOfBirth = dateOfBirth;
+    }
+
+    public void setCitizenID(String citizenID) {
+        CitizenID = citizenID;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public void setAmount(double amount) {
+        Amount = amount;
+    }
 }
