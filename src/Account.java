d6c7d640e5015e4d979e6238e3d626376143021b
levantiendat <levan040669@gmail.com>
2023-04-24 18:26:18 +0700

    commit class

--- src/Account.java
+++ src/Account.java
@@ -1,7 +1,12 @@
+import java.util.Calendar;
+
 public class Account {
     private String CardID;
     private String Password;
     private String Name;
+    private Calendar DateOfBirth;
+    private String CitizenID;
+    private String Type;
     private double Amount;
 
 }
